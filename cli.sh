a73f78d54d7db591d09a0cb81032310e19ab2218
Darkk-kami <71535947+Darkk-kami@users.noreply.github.com>
2024-08-09 09:06:31 +0100

    Created a bash script to automate the cli installation

---
+++ cli.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Check if the script is being run as root
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" 
+   exit 1
+fi
+
+sudo chmod +x ~/substream.py
+sudo mv ~/substream.py /usr/local/bin/substream
